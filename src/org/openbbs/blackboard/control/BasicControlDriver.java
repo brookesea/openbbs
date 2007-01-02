b97ebae0883536ff0e4193c06db9ff57e2b3672e
stefankst <stefankst@546f670b-8b20-0410-90a5-9283a50feb87>
2007-01-02 08:46:45 +0000

    * a BlackboardControl can close it's zone when it terminates

--- src/org/openbbs/blackboard/control/BasicControlDriver.java
+++ src/org/openbbs/blackboard/control/BasicControlDriver.java
@@ -15,45 +15,40 @@ public abstract class BasicControlDriver implements ControlDriver
    private List<BlackboardControl> controls = new ArrayList<BlackboardControl>();
    private boolean isRunning = false;
 
-   public void attachControl(BlackboardControl control)
-   {
+   public void attachControl(BlackboardControl control) {
       Validate.notNull(control);
 
       if (this.controls.contains(control)) return;
       this.controls.add(control);
    }
 
-   public void detachControl(BlackboardControl control)
-   {
+   public void detachControl(BlackboardControl control) {
       Validate.notNull(control);
       this.controls.remove(control);
    }
 
-   public List<BlackboardControl> attachedControls()
-   {
+   public List<BlackboardControl> attachedControls() {
       return Collections.unmodifiableList(new ArrayList<BlackboardControl>(this.controls));
    }
 
-   public boolean isRunning()
-   {
+   public boolean isRunning() {
       return this.isRunning;
    }
 
-   protected void runLoop()
-   {
+   protected void runLoop() {
       Validate.isTrue(!this.isRunning, "driver is already in run loop");
 
       this.isRunning = true;
       try {
          while (this.nextRun())
             ;
-      } finally {
+      }
+      finally {
          this.isRunning = false;
       }
    }
 
-   protected boolean nextRun()
-   {
+   protected boolean nextRun() {
       boolean continueRun = false;
 
       // clone control list to allow concurrent attach and detach
